a82f5e095048505a5f13b2dd64539eb1fa4e98a4
ljt <leprechaunlucas@gmail.com>
2013-02-14 09:49:00 -0800

    fix

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -33,12 +33,13 @@ var EmployeeView = function(employee) {
             return;
         }
         var contact = navigator.contacts.create();
-        contact.name = {givenName: app.employees.firstName, familyName:  app.employees.lastName};
+        contact.name = {givenName: app.employee.firstName, familyName:  app.employee.lastName};
         var phoneNumbers = [];
-        phoneNumbers[0] = new ContactField('work', app.employees.officePhone, false);
-        phoneNumbers[1] = new ContactField('mobile', app.employees.cellPhone, true); // preferred number
+        phoneNumbers[0] = new ContactField('work', app.employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', app.employee.cellPhone, true); // preferred number
         contact.phoneNumbers = phoneNumbers;
         contact.save();
+		 app.showAlert("contact added", "Contacts");
         return false;
     };
 
